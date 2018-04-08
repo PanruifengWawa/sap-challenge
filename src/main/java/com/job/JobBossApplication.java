163b6e65019c759387c18ffbb05d318f2dbf00a6
PanruifengWawa <46937922@qq.com>
2018-04-08 16:05:32 +0800

    init

---
+++ src/main/java/com/job/JobBossApplication.java
@@ -0,0 +1,12 @@
+package com.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBossApplication.class, args);
+	}
+}
